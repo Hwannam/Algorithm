5e8f872133695cd1108b614db49be35752477c3d
Hwannam <kyem0910@gmail.com>
2021-07-17 23:00:53 +0900

    0717

--- This is Coding Test/Greedy/3-2.py
+++ This is Coding Test/Greedy/3-2.py
@@ -1,16 +1,34 @@
-n,m,k = map(intm input().split())
+# 큰 수의 법칙-+
+n, m, k = map(int, input().split())
 
 data = list(map(int,input().split()))
 
-data.sort()
+data.sort() #오름차순
 first = data[n-1]
 second = data[n-2]
 
-count = int(m/(k+1))*k
-count += m%(k+1)
-
 result = 0
-result += (count) * first
-result += (m-count) * second
+
+while True:
+    for i in range(k):
+        if m ==0:
+            break
+        result = result + first 
+        m -=1 
+
+    if(m==0):
+        break
+    result = result + second
+    m -=1
+
+# 시간 초과 안뜨는 방법
+# count = int(m/(k+1)) *k
+# count += m%(k+1)
+
+# result = 0
+
+# result += count *first
+# result += (m-count) *second    
 
 print(result)
+
